8d61c362fa3138b5d981e6da2bba00d339142501
Ruben <vim.ruben@hotmail.com>
2020-07-02 21:05:26 -0300

    Creación y definición de estilos de landing page

---
+++ js/js.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    var scroll = $(window).scrollTop();
+    $("#posicion").text(scroll);
+    $(window).scroll(function (event) {
+      var scroll = $(window).scrollTop();
+      $("#posicion").text(scroll);
+      /*Cambia el color del div cuando es distinto a 0*/
+      if (scroll != 0) {
+        $("#nav-bar").css({
+          background: "rgba(38, 38, 38, 0.95)",
+        });
+      } else {
+        $("#nav-bar").css({
+          background: "transparent",
+        });
+      }
+    });
+  });
+
+  (function($){
+    $(function(){
+  
+      $('.sidenav').sidenav();
+      $('.parallax').parallax();
+  
+    }); // end of document ready
+  })(jQuery); // end of jQuery name space
